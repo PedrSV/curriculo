bf647ecf74464b4522ef09190f8722d4da439a57
Pedro Lucas Soares da Silva <110911485+PedrSV@users.noreply.github.com>
2022-11-25 21:35:31 -0400

    Update redir.js

--- redir.js
+++ redir.js
@@ -4,23 +4,23 @@ if(link.get('')) {
         case 'portalfamepi':
             window.location.href = 'http://inscricoes.fundacaomatiasmachline.org.br/FrameHTML/web/app/edu/PortalEducacional/login/'; break;
         case 'famepi':
-            window.location.href = 'https://www.famepi.org.br'; break;
+            window.location.href = 'https://www.famepi.org.br'; window.close(); break;
         case 'github':
-            window.location.href = 'http://github.com/pedrsv'; break;
+            window.location.href = 'http://github.com/pedrsv'; window.close(); break;
         case 'google':
-            window.location.href = 'http://www.google.com.br'; break;
+            window.location.href = 'http://www.google.com.br'; window.close(); break;
         case 'instagram':
-            window.location.href = 'https://www.instagram.com/pedrsv/'; break;
+            window.location.href = 'https://www.instagram.com/pedrsv/'; window.close(); break;
         case 'denver':
-            window.location.href = 'https://joaofefes.github.io/denver/'; break;
+            window.location.href = 'https://joaofefes.github.io/denver/'; window.close(); break;
         case 'linkedin':
-            window.location.href = 'https://br.linkedin.com/in/pedrolucas-sds'; break;
+            window.location.href = 'https://br.linkedin.com/in/pedrolucas-sds'; window.close(); break;
         default:
             // document.getElementById('msg').remove();
             // document.write('<p style="color:red;">Inválido. Verifique o link.</p>');
             alert('Não foi possível redirecionar para "' + link.get('') + '".\nVerifique o link e tente novamente.');
             if (confirm('hellooooo') == true) {
-                window.location.href = 'about:blank';
+                window.location.href = document.referrer;
             } else {window.location.href = 'about:blank';}
     }
 }
