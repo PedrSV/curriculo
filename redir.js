583346b6a5eca44c6ffcfcd1cf892e266c972dcf
Pedro Lucas Soares da Silva <110911485+PedrSV@users.noreply.github.com>
2022-12-09 01:35:46 -0400

    Update redir.js

--- redir.js
+++ redir.js
@@ -2,22 +2,22 @@
 const link = new URLSearchParams(document.location.search);
 if(link.get('')) {
     switch(link.get('')){
-        case 'portalfamepi':
-            window.location.href = 'http://inscricoes.fundacaomatiasmachline.org.br/FrameHTML/web/app/edu/PortalEducacional/login/'; break;
-        case 'famepi':
-            window.location.href = 'https://www.famepi.org.br'; break;
         case 'github':
             window.location.href = 'http://github.com/pedrsv'; break;
-        case 'google':
-            window.location.href = 'http://www.google.com.br'; break;
-        case 'instagram':
-            window.location.href = 'https://www.instagram.com/pedrsv/'; break;
-        case 'denver':
-            window.location.href = 'https://joaofefes.github.io/denver/'; break;
         case 'linkedin':
             window.location.href = 'https://br.linkedin.com/in/pedrolucas-sds'; break;
         case 'links':
             window.location.href = 'https://pedrsv.github.io/links'; break;
+        case 'instagram':
+            window.location.href = 'https://www.instagram.com/pedrsv/'; break;
+        case 'denver':
+            window.location.href = 'https://joaofefes.github.io/denver/'; break;
+        case 'famepi':
+            window.location.href = 'https://www.famepi.org.br'; break;
+        case 'portalfamepi':
+            window.location.href = 'http://inscricoes.fundacaomatiasmachline.org.br/FrameHTML/web/app/edu/PortalEducacional/login/'; break;
+        case 'oldportalfamepi':
+            window.location.href = 'http://inscricoes.fundacaomatiasmachline.org.br/Corpore.Net/Login.aspx?autoload=false&ReturnUrl=%2fCorpore.Net%2fMain.aspx%3fActionID%3dEduNotaEtapaActionWeb%26SelectedMenuIDKey%3dmnNotasEtapa'; break;
         default:
             if (confirm('Não foi possível redirecionar para "' + link.get('') + '".\nVerifique o link e tente novamente.')) {
                 window.location.href = 'about:blank'; // Em outro momento, testar com window.close();
